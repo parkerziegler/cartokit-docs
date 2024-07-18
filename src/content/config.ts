7390d6a7743611162a0cfb209a66a5c4ae27b6ea
parkerziegler <parkerellisziegler@gmail.com>
2024-07-17 17:08:10 -0700

    feat: Begin adding guides.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from "@astrojs/starlight/schema";
 // eslint-disable-next-line import/no-unresolved
-import { defineCollection } from 'astro:content';
+import { defineCollection } from "astro:content";
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() })
+  docs: defineCollection({ schema: docsSchema() }),
 };
