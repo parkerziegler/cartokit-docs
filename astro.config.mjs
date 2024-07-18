aa3d089636c7d5a095b9326de33ae7edeafa7906
parkerziegler <parkerellisziegler@gmail.com>
2024-07-18 10:56:11 -0700

    feat: Add Vercel analytics.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     starlight({
       title: "cartokit Docs",
